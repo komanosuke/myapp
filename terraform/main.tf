153cda80f4da9a575dba0615a41fc0e40b1f18da
komanosuke <toshihiko.komai616@gmail.com>
2023-07-25 12:13:04 +0900

    update circleci config.yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,9 @@
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
 provider "aws" {
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   region  = "us-west-1"
   profile = "default"
 }
